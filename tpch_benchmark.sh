1ab2a7b71a6ae6f419db2c6e23cb585ebfb095a0
Brandon Myers <bdmyers@cs.washington.edu>
2015-07-23 18:05:35 -0700

    use impala instead of hive shell because its SOOOOOOOOO much faster

--- tpch_benchmark.sh
+++ tpch_benchmark.sh
@@ -26,7 +26,9 @@ while [ $trial -lt $NUM_OF_TRIALS ]; do
 		echo "Running query: $query" | tee -a $LOG_FILE
 
 		echo "Running Hive prepare query: $query" >> $LOG_FILE
-		$TIME_CMD $HIVE_CMD -f $BASE_DIR/tpch_prepare/${query}.hive 2>&1 | tee -a $LOG_FILE | grep '^Time:'
+		#$TIME_CMD $HIVE_CMD -f $BASE_DIR/tpch_prepare/${query}.hive 2>&1 | tee -a $LOG_FILE | grep '^Time:'
+        # use impala instead of hive for DDL because its way faster
+		$TIME_CMD $IMPALA_CMD --query_file=$BASE_DIR/tpch_prepare/${query}.hive 2>&1 | tee -a $LOG_FILE | grep '^Time:'
                 returncode=${PIPESTATUS[0]}
 		if [ $returncode -ne 0 ]; then
 			echo "ABOVE QUERY FAILED:$returncode"
