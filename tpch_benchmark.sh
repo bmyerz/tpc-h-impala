6f54d8b46d2613203ea4e051ddbe04fd66a0d002
Brandon Myers <bdmyers@cs.washington.edu>
2015-08-07 11:02:14 -0700

    disabling code gen

--- tpch_benchmark.sh
+++ tpch_benchmark.sh
@@ -3,6 +3,7 @@
 # set up configurations
 source benchmark.conf;
 
+mkdir -p $LOG_DIR
 if [ -e "$LOG_FILE" ]; then
 	timestamp=`date "+%F-%R" --reference=$LOG_FILE`
 	backupFile="$LOG_FILE.$timestamp"
@@ -16,6 +17,11 @@ echo "***********************************************"
 echo "                                               " 
 echo "See $LOG_FILE for more details of query errors."
 echo ""
+
+if [[ -z $DISABLE_CODEGEN ]]; then
+    DISABLE_CODEGEN=0
+fi
+echo "Code generation disabled? $DISABLE_CODEGEN"
 
 trial=0
 while [ $trial -lt $NUM_OF_TRIALS ]; do
@@ -42,8 +48,22 @@ while [ $trial -lt $NUM_OF_TRIALS ]; do
 		#	echo "ABOVE QUERY FAILED:$returncode"
 		#fi
 
-		echo "Running Impala query: $query" >> $LOG_FILE
-		$TIME_CMD $IMPALA_CMD --query_file=$BASE_DIR/tpch_impala/${query}.impala 2>&1 | tee -a $LOG_FILE | grep '^Time:'
+        # add set codegen on/off to query code
+        querytmpdir=$BASE_DIR/tpch_impala/tmp
+        mkdir -p $querytmpdir
+        if [[ $DISABLE_CODEGEN -eq 1 ]]; then
+            querytmpdir=$BASE_DIR/tpch_impala/tmp
+            cat $BASE_DIR/disable_codegen_snippet.impala $BASE_DIR/tpch_impala/${query}.impala >$querytmpdir/${query}.impala
+        else
+            cp $BASE_DIR/tpch_impala/${query}.impala $querytmpdir/
+        fi
+            
+        echo "Running Impala query: $query" >> $LOG_FILE
+        $TIME_CMD $IMPALA_CMD --query_file=$querytmpdir/${query}.impala 2>&1 | tee -a $LOG_FILE | grep '^Time:'
+        echo "  -- and a second time: $query" >> $LOG_FILE
+        $TIME_CMD $IMPALA_CMD --query_file=$querytmpdir/${query}.impala 2>&1 | tee -a $LOG_FILE | grep '^Time:'
+        #$TIME_CMD $IMPALA_CMD --query_file=$BASE_DIR/tpch_impala/${query}-2.impala 2>&1 | tee -a $LOG_FILE | grep '^Time:'
+            
                 returncode=${PIPESTATUS[0]}
 		if [ $returncode -ne 0 ]; then
 			echo "ABOVE QUERY FAILED:$returncode"
