01a7a0219066eeeed000eb323463bc5d997823e3
Brandon Myers <bdmyers@cs.washington.edu>
2015-08-07 20:58:10 -0700

    fix naming of uploads

--- hdfs_upload.sh
+++ hdfs_upload.sh
@@ -1,16 +1,18 @@
-tablesdir=$HOME/escience/tpch_2_17_0/dbgen/sf10
+sf=100
+tablesdir=$HOME/escience/tpch_2_17_0/dbgen/sf$sf
 
 # turn the .tbl files into directories of 1 file (expected format for external tables)
-mkdir -p $tablesdir/hive
+mkdir -p $tablesdir/hive/$sf
 
 pushd $tablesdir
 for i in *.tbl; do
-    dir=hive/`basename $i .tbl`
+    dir=hive/$sf/`basename $i .tbl`
     mkdir -p $dir
     cp $i $dir/part-00000
+done
 popd
 
-hadoop fs -mkdir -p /user/bdmyers/tpch
-hadoop fs -copyFromLocal $tablesdir/hive /user/bdmyers/tpch/
+hadoop fs -mkdir -p /user/bdmyers/tpch/hive
+hadoop fs -copyFromLocal $tablesdir/hive/$sf /user/bdmyers/tpch/
 
 
